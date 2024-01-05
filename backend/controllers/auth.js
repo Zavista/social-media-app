8ec3ea3a7c4f298b3578ecefdf92f187fc63a2a8
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-05 17:46:36 -0500

    create login control

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -39,3 +39,27 @@ export const register = async (req, res) => {
         res.status(500).json({error: err.message})
     }
 }
+
+
+/* Logging In */
+export const login = async (res, req) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({email: email}); //emails are unique
+        if (!user) {
+            return res.status(400).json({msg: "User does not exist. "})
+        }
+        
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch){
+            return res.status(400).json({mesg: "Invalid credentials. "})
+        }
+
+        const token = jwt.sign({ id: user._id}, procss.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({token, user})
+  
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+}
