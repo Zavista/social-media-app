19355e783d7549beaf291ed34ba44cd2b0b0828f
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-06 15:35:57 -0500

    Create Desktop and Mobile Navbar

--- frontend/src/pages/navbar/Navbar.jsx
+++ frontend/src/pages/navbar/Navbar.jsx
@@ -8,8 +8,8 @@ import {
   MenuItem,
   FormControl,
   useTheme,
-  useMediaQuery
-} from '@mui/material'
+  useMediaQuery,
+} from "@mui/material";
 import {
   Search,
   Message,
@@ -17,21 +17,21 @@ import {
   LightMode,
   Notifications,
   Help,
-  Menu, 
-  Close
-} from '@mui/icons-material'
-import { useDispatch, useSelector } from 'react-redux';
-import { setMode, setLogut } from '../../state/state.js';
-import { useNavigate } from 'react-router-dom';
-import FlexBetween from '../../components/FlexBetween.jsx'
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "../../state/state.js";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "../../components/FlexBetween.jsx";
 
 const Navbar = () => {
   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
-  const isNonMobileScreens = useMediaQuery('(min-width: 1000px');
-  
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px");
+
   const theme = useTheme();
   const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.dark;
@@ -44,22 +44,137 @@ const Navbar = () => {
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
       <FlexBetween gap="1.75rem">
-        <Typography 
-        fontWeight='bold' 
-        fontSize='clamp(1rem, 2rem, 2.25rem' 
-        color='primary' 
-        onClick={() => navigate('/home')}
-        sx={{
-          "&:hover" : {
-            color: primaryLight,
-            cursor: 'pointer'
-          }
-        }}>
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem, 2rem, 2.25rem"
+          color="primary"
+          onClick={() => navigate("/home")}
+          sx={{
+            "&:hover": {
+              color: primaryLight,
+              cursor: "pointer",
+            },
+          }}
+        >
           Social Media App
         </Typography>
+        {isNonMobileScreens && (
+          <FlexBetween
+            backgroundColor={neutralLight}
+            borderRadius="9px"
+            gap="3rem"
+            padding="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..."></InputBase>
+            <IconButton>
+              <Search></Search>
+            </IconButton>
+          </FlexBetween>
+        )}
+
+        {isNonMobileScreens ? (
+          <FlexBetween gap="2rem">
+            <IconButton onClick={() => dispatch(setMode())}>
+              {theme.palette.mode === "dark" ? (
+                <DarkMode sx={{ fontSize: "25px" }}></DarkMode>
+              ) : (
+                <LightMode sx={{ color: "dark", fontSize: "25px" }}></LightMode>
+              )}
+            </IconButton>
+            <Message sx={{ fontSize: "25px" }}></Message>
+            <Help sx={{ fontSize: "25px" }}></Help>
+            <FormControl variant="standard" value={fullName}>
+              <Select
+                value={fullName}
+                sx={{
+                  backgroundColor: neutralLight,
+                  width: "150px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1 rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "3rem",
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neutralLight,
+                  },
+                }}
+              >
+                input={<InputBase></InputBase>}
+              </Select>
+            </FormControl>
+          </FlexBetween>
+        ) : (
+          <IconButton
+            onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+          >
+            <Menu></Menu>
+          </IconButton>
+        )}
+
+        {!isNonMobileScreens && isMobileMenuToggled && (
+          <Box
+            position="fixed"
+            right="0"
+            bottom="0"
+            height="100%"
+            zIndex="10"
+            maxWidth="500px"
+            minWidth="300px"
+            backgroundColor={background}
+          >
+            <Box display="flex" justifyContent="flex-end" p="1rem">
+              <IconButton
+                onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+              >
+                <Close></Close>
+              </IconButton>
+            </Box>
+
+            <FlexBetween
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              gap="3rem"
+            >
+              <IconButton sx={{ fontSize: "25px" }} onClick={() => dispatch(setMode())}>
+                {theme.palette.mode === "dark" ? (
+                  <DarkMode sx={{ fontSize: "25px" }}></DarkMode>
+                ) : (
+                  <LightMode
+                    sx={{ color: "dark", fontSize: "25px" }}
+                  ></LightMode>
+                )}
+              </IconButton>
+              <Message sx={{ fontSize: "25px" }}></Message>
+              <Help sx={{ fontSize: "25px" }}></Help>
+              <FormControl variant="standard" value={fullName}>
+                <Select
+                  value={fullName}
+                  sx={{
+                    backgroundColor: neutralLight,
+                    width: "150px",
+                    borderRadius: "0.25rem",
+                    p: "0.25rem 1 rem",
+                    "& .MuiSvgIcon-root": {
+                      pr: "0.25rem",
+                      width: "3rem",
+                    },
+                    "& .MuiSelect-select:focus": {
+                      backgroundColor: neutralLight,
+                    },
+                  }}
+                >
+                  input={<InputBase></InputBase>}
+                </Select>
+              </FormControl>
+            </FlexBetween>
+          </Box>
+        )}
       </FlexBetween>
     </FlexBetween>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
