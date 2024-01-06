7fd17715e0dcbd9262870230f4bbc8ea63daafab
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-06 16:01:45 -0500

    Create login header

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,6 +1,35 @@
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material'
+
+
 const Login = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery('(mind-width: 1000px');
+
+
+
   return (
-    <div>Login</div>
+    <Box>
+      <Box width='100%' backgroundColor={theme.palette.background.alt}>
+        <Typography
+            fontWeight="bold"
+            fontSize="32px"
+            color="primary">
+            Fakebook
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p='2rem'
+        m='2rem auto'
+        borderRadius='1.5rem'
+        backgroundColor={theme.palette.background.alt}
+        >
+          <Typography fontWeight='500' variant='h5' sx={{mb: '1.5rem'}}>
+            Welcome to Fakebook, a Facebook clone!
+          </Typography>
+      </Box>
+    </Box>
   )
 }
 
