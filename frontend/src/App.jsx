e82b4596c387e50ad7136440839923865c7e6843
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-05 23:00:14 -0500

    Add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home.jsx'
+import Login from './pages/login/Login.jsx'
+import Profile from './pages/profile/Profile.jsx'
+
 const App = () => {
+
+
   return (
-    <div>App</div>
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login></Login>}></Route>
+          <Route path='/home' element={<Home></Home>}></Route>
+          <Route path='/profile/:userId' element={<Profile></Profile>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
