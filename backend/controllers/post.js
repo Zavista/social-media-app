8485fded1c46ff589c9f7d59ceb22f72e4803318
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-05 19:14:42 -0500

    Create createPost control

---
+++ backend/controllers/post.js
@@ -0,0 +1,31 @@
+import Post from '../models/Post.js'
+import User from '../models/User.js';
+
+
+/* Create */
+export const createPost = async (req, res) => {
+    try {
+        const { userId, description, picturePath } = req.body;
+        const user = await User.findById(userId);
+        const newPost = new Post({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            description,
+            userPicturePath: user.picturePath,
+            picturePath,
+            likes: {},
+            comments: []
+        })
+
+        await newPost.save();
+
+        //return list of all post with new post for front-end
+        const post = await Post.find();
+        res.status(201).json(post);
+
+    } catch (err) {
+        res.status(409).json({message: err.message});
+    }
+}
