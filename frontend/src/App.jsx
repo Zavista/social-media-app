7d8daec6d19ff75e4b7e910c4fa843ebf6c9fe6c
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-06 00:23:49 -0500

    Import color and theme and add them to components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,27 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home.jsx'
 import Login from './pages/login/Login.jsx'
 import Profile from './pages/profile/Profile.jsx'
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { CssBaseline, ThemeProvider, createTheme} from '@mui/material'
+import { themeSettings } from './theme.js'
 
 const App = () => {
 
+  const mode = useSelector((state) => (state.mode));
+  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode])
 
   return (
     <div className='app'>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login></Login>}></Route>
-          <Route path='/home' element={<Home></Home>}></Route>
-          <Route path='/profile/:userId' element={<Profile></Profile>}></Route>
-        </Routes>
+          <ThemeProvider theme={theme}>
+            <CssBaseline></CssBaseline>
+            <Routes>
+              <Route path='/' element={<Login></Login>}></Route>
+              <Route path='/home' element={<Home></Home>}></Route>
+              <Route path='/profile/:userId' element={<Profile></Profile>}></Route>
+            </Routes>
+          </ThemeProvider>
       </BrowserRouter>
     </div>
   )
