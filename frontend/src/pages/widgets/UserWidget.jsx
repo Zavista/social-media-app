ed2b0dd27af3e9ac26a83622ba034b51fad960a7
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-06 22:45:45 -0500

    UserWidget widget setup

---
+++ frontend/src/pages/widgets/UserWidget.jsx
@@ -0,0 +1,24 @@
+import {
+    ManageAccountsOutlined,
+    EditOutlined,
+    LocationOnOutlined,
+    WorkOutlineOutlined,
+} from '@mui/icons-material'
+import { Box, Typography, Divider, useTheme} from '@mui/material'
+import UserImage from '../../components/UserImage.jsx'
+import FlexBetweeb from '../../components/FlexBetween.jsx'
+import WidgetWrapper from '../../components/WidgetWrapper.jsx'
+import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+const UserWidget = ({ userId, picturePath}) => {
+    
+
+  return (
+    <div>UserWidget</div>
+  )
+}
+
+export default UserWidget
