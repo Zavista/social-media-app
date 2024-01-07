ab335e40e69fbade1d39066f1628c79af04ce265
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-06 23:56:50 -0500

    Add userWidget to Home

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,28 @@
-import { Box } from "@mui/material"
+import { Box, useMediaQuery } from "@mui/material"
+import { useSelector } from "react-redux"
 import Navbar from "../navbar/Navbar.jsx"
+import UserWidget from "../widgets/UserWidget.jsx"
 
 const Home = () => {
+
+  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
+  const { _id, picturePath} = useSelector((state) => state.user);
+
+
   return (
     <Box>
       <Navbar></Navbar>
+      <Box
+        width='100%'
+        padding='2rem 6%'
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap='0.5rem'
+        justifyContent='space-between'
+      >
+        <Box flexBasis={isNonMobileScreens ? '26%' : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath}></UserWidget>
+        </Box>
+      </Box>
     </Box>
   )
 }
