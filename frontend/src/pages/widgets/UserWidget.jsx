e158f84db9f7831337b15ce6bad7ae3284b7637b
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-06 23:06:41 -0500

    Set up UserWidget to get User

--- frontend/src/pages/widgets/UserWidget.jsx
+++ frontend/src/pages/widgets/UserWidget.jsx
@@ -14,7 +14,42 @@ import { useNavigate } from 'react-router-dom'
 
 
 const UserWidget = ({ userId, picturePath}) => {
+    const [user, setUser] = useState(null);
+    const palette = useTheme();
+    const navigate = useNavigate();
+    const token = useSelector((state) => state.token);
+    const dark = palette.neutral.dark;
+    const medium = palette.neutral.medium;
+    const main = palette.neutral.main;
+
+    const getUser = async () => {
+        const response = await fetch(`http://localhost:3002/users/${userId}`,
+        {
+            method: 'GET',
+            headers: { Authorization: `Bearer ${token}`}
+
+        })
+        const data = await response.json;
+        setUser(data);
+    }
+
+    useEffect(()=>{
+        getUser()
+    },[])
+
+    if (!user) {
+        return null;
+    }
     
+    const {
+        firstName,
+        lastName,
+        location,
+        occupation,
+        viewedProfile,
+        impressions,
+        friends
+    } = user;
 
   return (
     <div>UserWidget</div>
