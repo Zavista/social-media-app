d35f53a723edf526062a15723fef5ee557a8287e
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-06 23:14:56 -0500

    Created userwidget second row

--- frontend/src/pages/widgets/UserWidget.jsx
+++ frontend/src/pages/widgets/UserWidget.jsx
@@ -81,7 +81,17 @@ const UserWidget = ({ userId, picturePath}) => {
 
             <Divider></Divider>
 
-            
+            <Box p='1rem 0'>
+                <Box display='flex' alignItems='center' gap='1rem' mb='0.5rem'>
+                    <LocationOnOutlined fontSize='large' sx={{color: main}}></LocationOnOutlined>
+                    <Typography color={medium}>{location}</Typography>
+                </Box>
+                <Box display='flex' alignItems='center' gap='1rem'>
+                    <WorkOutlineOutlined fontSize='large' sx={{color: main}}></WorkOutlineOutlined>
+                    <Typography color={medium}>{occupation}</Typography>
+                </Box>
+            </Box>
+
         </FlexBetween>
     </WidgetWrapper>
   )
