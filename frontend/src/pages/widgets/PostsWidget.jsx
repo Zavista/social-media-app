c97bb4f704865fb7444f15e95fbbe4bc0e0e87dd
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-07 23:48:03 -0500

    Create feed by mapping the posts with postWidget

--- frontend/src/pages/widgets/PostsWidget.jsx
+++ frontend/src/pages/widgets/PostsWidget.jsx
@@ -25,9 +25,47 @@ const PostsWidget = ({ userId,  isProfile = false}) => {
         dispatch(setPosts({posts: data}))
     }
 
-    
+    useEffect(()=>{
+        if (isProfile) {
+            getUserPosts();
+        }
+        else{
+            getPosts();
+        }
+    }, [])
+
   return (
-    <div>PostsWidget</div>
+    <>
+        {posts.map(
+            ({
+                _id,
+                userId,
+                firstName,
+                lastName,
+                description,
+                location,
+                picturePath,
+                userPicturePath,
+                likes,
+                comments,
+            })=> (
+                <PostsWidget
+                    key={_id}
+                    postId={_id}
+                    postUserId={userId}
+                    name={`${firstName} ${lastName}`}
+                    description={description}
+                    location={location}
+                    picturePath={picturePath}
+                    userPicturePath={userPicturePath}
+                    likes={likes}
+                    comments={comments}
+                >
+
+                </PostsWidget>
+            )
+        )}
+    </>
   )
 }
 
