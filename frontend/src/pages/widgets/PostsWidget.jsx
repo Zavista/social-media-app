aa8f6eefb0f55899bb7a0dfa913ab2edc3a0e4bb
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-07 23:35:41 -0500

    created functions to call backend

--- frontend/src/pages/widgets/PostsWidget.jsx
+++ frontend/src/pages/widgets/PostsWidget.jsx
@@ -7,7 +7,25 @@ const PostsWidget = ({ userId,  isProfile = false}) => {
     const posts = useSelector((state) => state.posts);
     const token = useSelector((state) => state.token);
 
+    const getPosts = async () => {
+        const response = await fetch('http://localhost:3002/posts', {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}`}
+        })
+        const data = await response.json();
+        dispatch(setPosts({posts: data}))
+    }
 
+    const getUserPosts = async () => {
+        const response = await fetch(`http://localhost:3002/posts/${userId}`, {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}`}
+        })
+        const data = await response.json();
+        dispatch(setPosts({posts: data}))
+    }
+
+    
   return (
     <div>PostsWidget</div>
   )
