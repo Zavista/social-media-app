375e03c0e5347100b47971bac392c8ba1f0ec8f9
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-07 21:35:33 -0500

    Create myPostWidget and setup

--- frontend/src/pages/widgets/MyPostWidget.jsx
+++ frontend/src/pages/widgets/MyPostWidget.jsx
@@ -1,5 +1,47 @@
+import {
+    EditOutlined,
+    DeleteOutlined,
+    AttachFileOutlined,
+    GifBoxOutlined,
+    ImageOutlined,
+    MicOutlined,
+    MoreHorizOutlined,
+} from '@mui/icons-material'
+import { 
+    Box,  
+    Divider, 
+    Typography, 
+    InputBase, 
+    useTheme, 
+    Button, 
+    IconButton, 
+    useMediaQuery
+} from '@mui/material'
+import FlexBetween from '../../components/FlexBetween.jsx'
+import Dropzone from 'react-dropzone'
+import UserImage from '../../components/UserImage.jsx'
+import WidgetWrapper from '../../components/WidgetWrapper.jsx'
+import { useState } from 'react'
+import { useDispatch,  useSelector } from 'react-redux'
+import { setPosts } from '../../state/state.js'
+
+
+
 
 const MyPostWidget = () => {
+
+    const dispatch = useDispatch();
+    const [isImage, setIsImage] = useState(false);
+    const [image, setImage] = useState(null);
+    const [post, setPost] = useState('')
+    const { palette } =  useTheme();
+    const id = useSelector((state) => state.user.id);
+    const token = useSelector((state) => state.token);
+    const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
+    const mediumMain = palette.neutral.mediumMain;
+    const medium = palette.neutral.medium;
+
+
   return (
     <div>MyPostWidget</div>
   )
