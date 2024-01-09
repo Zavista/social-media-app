ce47d3e90ea4c4b387c3ee65d1aa45708231caa0
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-08 21:03:20 -0500

    Add firstName value

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -7,6 +7,10 @@ const PostSchema = mongoose.Schema(
             type: String,
             required: true,
         },
+        firstName: {
+            type: String,
+            required: true,
+        },
         lastName: {
             type: String,
             required: true,
