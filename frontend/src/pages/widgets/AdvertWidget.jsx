b7043b4d4e4d843e4d7f45d809f00969bada7bef
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-08 21:38:36 -0500

    Create advert html

--- frontend/src/pages/widgets/AdvertWidget.jsx
+++ frontend/src/pages/widgets/AdvertWidget.jsx
@@ -3,8 +3,36 @@ import FlexBetween from '../../components/FlexBetween.jsx';
 import WidgetWrapper from '../../components/WidgetWrapper.jsx';
 
 const AdvertWidget = () => {
+    const { palette } = useTheme();
+    const dark = palette.neutral.dark;
+    const main = palette.neutral.main;
+    const medium = palette.neutral.medium;
+
+
   return (
-    <div>AdvertWidget</div>
+    <WidgetWrapper>
+        <FlexBetween>
+            <Typography color={dark} variant="h5" fontWeight='500'>
+                Sponsored
+            </Typography>
+            <Typography color={medium}>Create Ad</Typography>
+        </FlexBetween>
+        <img
+            width='100%'
+            height='auto'
+            alt='advert'
+            src='http://localhost:3002/assets/info2.jpeg'
+            style={{borderRadius: '0.75rem', margin:'0.75rem 0'}}
+        >
+        </img>
+        <FlexBetween>
+            <Typography color={main}>KFC</Typography>
+            <Typography color={medium}>kfc.com</Typography>
+        </FlexBetween>
+        <Typography color={medium} m='0.5rem 0'>
+            Indulge in KFC&apos;s iconic flavors today! Don&apos;t miss out! Come savor happiness in every bite! Visit us now and treat yourself! 
+        </Typography>
+    </WidgetWrapper>
   )
 }
 
