cfa54a6fb9c99f5b9ca128890a2c1c724371f2ab
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-08 21:32:26 -0500

    Create and setup  advert widget

---
+++ frontend/src/pages/widgets/AdvertWidget.jsx
@@ -0,0 +1,11 @@
+import { Typography, useTheme } from '@mui/material';
+import FlexBetween from '../../components/FlexBetween.jsx';
+import WidgetWrapper from '../../components/WidgetWrapper.jsx';
+
+const AdvertWidget = () => {
+  return (
+    <div>AdvertWidget</div>
+  )
+}
+
+export default AdvertWidget
