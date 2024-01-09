9930f86b9ca5c018f2ef09b8d448abf9d29ee7ad
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-08 19:08:25 -0500

    Create and setup PostWidget

---
+++ frontend/src/pages/widgets/PostWidget.jsx
@@ -0,0 +1,14 @@
+import { ChatBubbleOutlineOutlined, FavouriteBorderOutlined, FavouriteOutlined, ShareOutlined} from  '@mui/icons-material'
+import { Box, Divider, IconButton, Typography, useTheme } from '@mui/material'
+import FlexBetween from '../../components/FlexBetween.jsx'
+import Friend from '../../components/Friend.jsx'
+
+
+
+const PostWidget = () => {
+  return (
+    <div>PostWidget</div>
+  )
+}
+
+export default PostWidget
