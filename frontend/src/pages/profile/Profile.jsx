157706373993d763575726dd4aa0562768c89ba8
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-08 22:16:02 -0500

    Create Profile page html

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 import Navbar from '../navbar/Navbar';
 import FriendListWidget from '../widgets/FriendListWidget';
 import MyPostWidget from '../widgets/MyPostWidget';
-import PostWidget from '../widgets/PostWidget';
+import PostsWidget from '../widgets/PostsWidget';
 import UserWidget from '../widgets/UserWidget';
 
 
@@ -16,7 +16,7 @@ const Profile = () => {
   const isNonMobileScreens = useMediaQuery('(min-width: 1000px');
 
   const getUser = async () => {
-    const response = await fetch(`http://localhost:3002/users${userId}`, {
+    const response = await fetch(`http://localhost:3002/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}`}
     })
@@ -28,8 +28,36 @@ const Profile = () => {
     getUser()
   },[])
 
+  if (!user) return null;
+
   return (
-    <div>Profile</div>
+    <Box>
+      <Navbar></Navbar>
+      <Box
+        width='100%'
+        padding='2rem 6%'
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap='2rem'
+        justifyContent='center'
+      >
+        <Box flexBasis={isNonMobileScreens ? '26%' : undefined}>
+          <UserWidget userId={user._id} picturePath={user.picturePath}></UserWidget>
+          <Box m='2rem 0'></Box>
+          <FriendListWidget userId={userId}></FriendListWidget>
+        </Box>
+        
+        <Box 
+          flexBasis={isNonMobileScreens ? '42%' : undefined}
+          mt={isNonMobileScreens ? undefined : '2rem'}
+        >
+          <MyPostWidget picturePath={user.picturePath}></MyPostWidget>
+          <Box m='2rem 0'></Box>
+          <PostsWidget userId={user._id} isProfile></PostsWidget>
+        </Box>
+      </Box>
+
+      
+    </Box>
   )
 }
 
