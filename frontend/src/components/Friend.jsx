1f7c40011500ade126bb6c3e7bc125037fed3ebf
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-08 19:12:23 -0500

    Create and setup Friend component

---
+++ frontend/src/components/Friend.jsx
@@ -0,0 +1,15 @@
+import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
+import { Box, IconButton, Typography, useTheme } from '@mui/icons-material';
+import {  useDispatch, useSelector } from  'react-redux';
+import { setFriends } from '../state/state.js'
+import FlexBetween from "../components/FlexBetween.jsx";
+import UserImage from '../components/UserImage.jsx'
+
+
+const Friend = () => {
+  return (
+    <div>Friend</div>
+  )
+}
+
+export default Friend
