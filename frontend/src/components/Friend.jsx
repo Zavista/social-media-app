9addf336558a593d2f3d7e33bd7ecb18ccabab89
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-08 19:28:44 -0500

    Create function to get updated friends from backend

--- frontend/src/components/Friend.jsx
+++ frontend/src/components/Friend.jsx
@@ -6,7 +6,35 @@ import FlexBetween from "../components/FlexBetween.jsx";
 import UserImage from '../components/UserImage.jsx'
 
 
-const Friend = () => {
+const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const _id= useSelector((state) => state.user._id);
+    const token = useSelector((state) => state.token);
+    const friends = useSelector((state) => state.user.friends);
+
+    const { palette } = useTheme();
+    const primaryLight = palette.primary.light;
+    const primaryDark = palette.primary.dark;
+    const main  = palette.neutral.main;
+    const medium = palette.neutral.medium;
+
+    const isFriend  = friends.find((friend) => friend._id  === friendId);
+
+    const patchFriend = async () => {
+        const response = await fetch(`http:/localhost:3002/users/${_id}/${friendId}`, 
+        {
+            method: "PATCH",
+            headers: {
+                Authorization:  `Bearer ${token}`,
+                "Content-Type": 'aplication/json'
+            }
+        })
+
+        const data = await response.json();
+        dispatch(setFriends({ friends: data}))
+    }
+
   return (
     <div>Friend</div>
   )
