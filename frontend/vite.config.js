7d54c2680270a68cbe8f3ae82cd2dda8e7cd49fb
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-09 19:55:57 -0500

    Removed build options

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['mongoose'], // Add mongoose as an external dependency
-    },
-  },
 });
