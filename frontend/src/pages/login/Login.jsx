2c0b7eeedf8dc85ca029718871d45c53ede4cd05
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-11 20:23:48 -0500

    Changed FakeBook to Socialize

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Fakebook
+          Socialize
         </Typography>
       </Box>
 
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Fakebook, a Facebook clone!
+          Welcome to Socialize, a Social Media platform!
         </Typography>
         <Form />
       </Box>
--- frontend/src/pages/navbar/Navbar.jsx
+++ frontend/src/pages/navbar/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Fakebook
+          Socialize
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
